4fcb46e34bf08886fd252176b200c98e7d7bd11d
Gaurav Kumar <mappleinfosystem@gmail.com>
2025-06-03 00:43:09 +0530

    update controller

--- src/main/java/com/gkcare/sec/controller/HomeController.java
+++ src/main/java/com/gkcare/sec/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.gkcare.sec.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +10,11 @@ public class HomeController {
 
     @GetMapping("/home")
     public String home(){
+        return "Welcome to Spring Boot Security";
+    }
+
+    @RequestMapping(method = RequestMethod.GET,path = "/test")
+    public String a(){
         return "Welcome to Spring Boot Security";
     }
 }
